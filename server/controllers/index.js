3459b83a73d0a76e31a3d8bd5a1022ffc0ff997b
Tony Bogun <antonybogun@gmail.com>
2017-03-31 01:07:24 -0400

    Add table scraping for movie parameters of first row

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -9,10 +9,21 @@ module.exports.scrapeMovieDetails = function (req, res) {
             let rows = $('table.chart.full-width>tbody.lister-list>tr');
 
             let cells = rows.eq(0).children('td');
-            //id
+            // id
             let pathname = cells.eq(1).find('a').attr('href').split('/');
             console.log("["+ pathname[2] + "]");
-            
+
+            // small poster
+            console.log("["+ cells.eq(0).find('img').attr('src') + "]");
+
+            // name
+            console.log("["+ cells.eq(1).find('a').text() + "]");
+
+            // year
+            console.log("["+ cells.eq(1).find('.secondaryInfo').text().replace('(','').replace(')','') + "]");
+
+            // rating
+            console.log("["+ cells.eq(2).find('strong').text() + "]");
         }
     });
 };
