5f2e19937419563fee02948e38c96058beffd414
Tony Bogun <antonybogun@gmail.com>
2017-03-31 13:35:37 -0400

    Enable CORS for response

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -39,6 +39,9 @@ function scraper(req, res, url, arrayName) {
             }
             let jsonRes = {};
             jsonRes[arrayName] = mostPopularMovies;
+
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             res.status(200).json(jsonRes);
         }
     });
